1fc52a50725ef2bc13aa16bbedc39d0432f0ae24
AlexDoss42 <alex.doss42@gmail.com>
2020-10-02 14:55:56 -0600

    sets up a basic server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`It's over Anakin. I have the ${PORT} PORT!`));

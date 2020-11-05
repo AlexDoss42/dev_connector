1ab645669de40cb8b019620ce82c9e19b4e4d6aa
AlexDoss42 <alex.doss42@gmail.com>
2020-11-05 15:34:55 -0700

    front end for all profiles

--- client/src/components/profile-form/AddExperience.js
+++ client/src/components/profile-form/AddExperience.js
@@ -1,5 +1,4 @@
 import React, { useState, Fragment } from 'react';
-import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addExperience } from '../../actions/profile';

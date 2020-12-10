1370c958e75f179d1ee2c5dc9554549af9fc5520
AlexDoss42 <alex.doss42@gmail.com>
2020-12-10 15:06:04 -0700

    adds education to profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 
@@ -27,7 +28,6 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
           <ProfileAbout profile={profile} />
           <div className='profile-exp bg-white p-2'>
           <h2 class="text-primary">Experience</h2>
-          <div>
             {profile.experience.length > 0 ? (
               <Fragment>
                 {profile.experience.map(exp => {
@@ -36,6 +36,15 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
               </Fragment>
             ) : (<h4>No Experience Credentials</h4>)}
           </div>
+          <div className='profile-edu bg-white p-2'>
+          <h2 class="text-primary">Education</h2>
+            {profile.education.length > 0 ? (
+              <Fragment>
+                {profile.education.map(edu => {
+                  <ProfileEducation key={education._id} education={education} />
+                })}
+              </Fragment>
+            ) : (<h4>No Education Credentials</h4>)}
           </div>
         </div>
     </Fragment>

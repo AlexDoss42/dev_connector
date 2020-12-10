627d448c49bb2917f7450769fa44ad02ef3a45f9
AlexDoss42 <alex.doss42@gmail.com>
2020-12-10 15:00:34 -0700

    adds experience

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 import { getProfileById } from '../../actions/profile';
 import { Link } from 'react-router-dom';
 
@@ -24,6 +25,18 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
         <div class="profile-grid my-1">
           <ProfileTop profile={profile} />
           <ProfileAbout profile={profile} />
+          <div className='profile-exp bg-white p-2'>
+          <h2 class="text-primary">Experience</h2>
+          <div>
+            {profile.experience.length > 0 ? (
+              <Fragment>
+                {profile.experience.map(exp => {
+                  <ProfileExperience key={experience._id} experience={experience} />
+                })}
+              </Fragment>
+            ) : (<h4>No Experience Credentials</h4>)}
+          </div>
+          </div>
         </div>
     </Fragment>
   )

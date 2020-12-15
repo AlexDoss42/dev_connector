bbafd7cb498994ae290d5d32b975ac56dad77b4a
AlexDoss42 <alex.doss42@gmail.com>
2020-12-14 17:07:03 -0700

    adds logic to add a post

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -3,7 +3,8 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from '../actions/types';
 
 const initialState = {
@@ -23,12 +24,18 @@ export default function(state = initialState, action) {
         posts: payload,
         loading: false
       };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [...state.posts, payload],
+        loading: false
+      };
     case DELETE_POST:
       return {
         ...state,
         posts: state.posts.filter(post => post._id !== payload),
         loading: false
-      }
+      };
     case POST_ERROR:
       return {
         ...state,
